431fc53224bcb670e56265cf0930f8541cb3c5b1
davidHG <gudrmsglgl@gmail.com>
2021-03-22 21:24:44 +0900

    NoteListVM NextPageExist Test

--- presentation/src/test/java/com/cleannote/presentation/notelist/NoteListViewModelTest.kt
+++ presentation/src/test/java/com/cleannote/presentation/notelist/NoteListViewModelTest.kt
@@ -3,10 +3,7 @@ package com.cleannote.presentation.notelist
 import android.content.SharedPreferences
 import com.cleannote.domain.Constants
 import com.cleannote.domain.interactor.usecases.common.DeleteNote
-import com.cleannote.domain.interactor.usecases.notelist.DeleteMultipleNotes
-import com.cleannote.domain.interactor.usecases.notelist.InsertNewNote
-import com.cleannote.domain.interactor.usecases.notelist.NoteListUseCases
-import com.cleannote.domain.interactor.usecases.notelist.SearchNotes
+import com.cleannote.domain.interactor.usecases.notelist.*
 import com.cleannote.presentation.test.InstantExecutorExtension
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
@@ -25,24 +22,26 @@ open class NoteListViewModelTest {
     private lateinit var deleteNote: DeleteNote
     private lateinit var deleteMultipleNotes: DeleteMultipleNotes
 
+    lateinit var nextPageExist: NextPageExist
+    private lateinit var queryManager: QueryManager
+
     @BeforeEach
     fun noteListViewModelSetup(){
         initMock()
         useCases = NoteListUseCases(searchNotes, insertNewNote, deleteNote, deleteMultipleNotes)
-        noteListViewModel = NoteListViewModel(useCases, sharedPreferences)
+        queryManager = QueryManager(sharedPreferences, nextPageExist)
+        noteListViewModel = NoteListViewModel(useCases, queryManager)
     }
 
     private fun initMock(){
-        sharedPreferences = mock{
-            on { getString(
-                Constants.FILTER_ORDERING_KEY,
-                Constants.ORDER_DESC
-            ) } doReturn Constants.ORDER_DESC
+        sharedPreferences = mock {
+            on { getString( Constants.FILTER_ORDERING_KEY, Constants.ORDER_DESC) } doReturn Constants.ORDER_DESC
         }
         searchNotes = mock()
         insertNewNote = mock()
         deleteNote = mock()
         deleteMultipleNotes = mock()
+        nextPageExist = mock()
     }
 
 
---
+++ presentation/src/test/java/com/cleannote/presentation/notelist/next/tester/NextFeatureTester.kt
@@ -0,0 +1,34 @@
+package com.cleannote.presentation.notelist.next.tester
+
+import com.cleannote.domain.interactor.usecases.notelist.NextPageExist
+import com.cleannote.domain.model.Query
+import com.cleannote.presentation.ViewModelFeatureTester
+import com.cleannote.presentation.data.DataState
+import com.cleannote.presentation.data.State
+import com.cleannote.presentation.extensions.verifyExecute
+import com.cleannote.presentation.notelist.NoteListViewModel
+
+class NextFeatureTester(
+    private val viewModel: NoteListViewModel,
+    private val useCase: NextPageExist,
+    private val nextUseCaseCaptors: NextUseCaseCaptors
+): ViewModelFeatureTester<NextFeatureTester, Boolean, Query, Boolean>(nextUseCaseCaptors) {
+
+    fun nextPageExist(): NextFeatureTester {
+        viewModel.updateNextPageExist()
+        setState(currentState())
+        return this
+    }
+
+    override fun verifyUseCaseExecute(): NextFeatureTester {
+        useCase.verifyExecute(nextUseCaseCaptors, nextUseCaseCaptors.paramCaptor)
+        return this
+    }
+
+    override fun vmCurrentData(): DataState<Boolean>? {
+        return viewModel.queryMgr.isNextPageExist()
+    }
+
+    override fun currentState(): State? = vmCurrentData()?.status
+
+}
